abbdb430fffce7e6ac21cc995661f88ab4c49993
Jef <jeffin41@gmail.com>
2022-05-27 00:25:36 +0530

    first commit

---
+++ src/test/java/Learning/Mavenjava/APITest.java
@@ -0,0 +1,19 @@
+package Learning.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test 
+	public void postJira() {
+		
+		System.out.println("post Jira");
+	}
+	
+	@Test 
+	public void deleteTwitter() {
+		
+		System.out.println("delete Twitter");
+	}
+
+}
---
+++ src/test/java/Learning/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package Learning.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void nativeAppAndroid() {
+		
+		System.out.println("Native App Android");
+	}
+	
+	@Test
+	public void nativeAppiOS() {
+		
+		System.out.println("nativeApp iOS");
+	}
+}
